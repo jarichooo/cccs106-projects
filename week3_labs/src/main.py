ba3298efc39f29b2d17aefec1a6ad29cec454c78
jarichooo <[jobarja@my.cspc.edu.ph]>
2025-09-20 07:36:13 +0800

    Add updated main.py and dc_connection.py

--- week3_labs/src/main.py
+++ week3_labs/src/main.py
@@ -1,26 +1,150 @@
 import flet as ft
-
+from db_connection import connect_db
+import mysql.connector
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+    # window settings
+    page.window.frameless = True
+    page.window.title = "User Login"
+    page.window.alignment = ft.alignment.center
+    page.window.height = 350
+    page.window.width = 400
+    page.bgcolor = ft.Colors.AMBER_ACCENT
+    page.theme_mode = ft.ThemeMode.LIGHT
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
+    login_title = ft.Text(
+        "User Login",
+        size=20,
+        weight=ft.FontWeight.BOLD,
+        font_family="Arial",
+        text_align=ft.TextAlign.CENTER,
     )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
+
+    username_field = ft.TextField(
+        label="User name",
+        hint_text="Enter your user name",
+        helper_text="This is your unique identifier",
+        width=300,
+        autofocus=True,
+        prefix_icon=ft.Icons.PERSON,
+        bgcolor=ft.Colors.LIGHT_BLUE_ACCENT,
+    )
+
+    password_field = ft.TextField(
+        label="Password",
+        hint_text="Enter your password",
+        helper_text="This is your secret key",
+        width=300,
+        password=True,                # Obscure text (password mode)
+        can_reveal_password=True,     # Allow revealing password
+        prefix_icon=ft.Icons.PASSWORD, # Password icon
+        bgcolor=ft.Colors.LIGHT_BLUE_ACCENT,
+    )
+
+    async def login_click(e):
+      
+        # dialogs
+        success_dialog = ft.AlertDialog(
+            modal=True,
+            title=ft.Text("Login Successful"),
+            content=ft.Text(f"Welcome! {username_field.value}"),
+            alignment=ft.alignment.center,
+            actions=[
+                ft.TextButton("OK", on_click=lambda e: page.close(success_dialog))
+            ], icon=ft.Icon(name=ft.Icons.CHECK_CIRCLE, color=ft.Colors.GREEN)
         )
+
+        failure_dialog = ft.AlertDialog(
+            modal=True,
+            title=ft.Text("Login Failed"),
+            content=ft.Text("Invalid username or password"),
+            alignment= ft.alignment.center,
+            actions=[
+                ft.TextButton("OK", on_click=lambda e: page.close(failure_dialog))
+            ],icon=ft.Icon(name=ft.Icons.ERROR, color=ft.Colors.RED),
+        )
+
+        invalid_input_dialog = ft.AlertDialog(
+            modal=True,
+            title=ft.Text("Input Error"),
+            content=ft.Text("Please enter username and password"),
+            alignment=ft.alignment.center,
+            actions=[
+                ft.TextButton("OK", on_click=lambda e: page.close(invalid_input_dialog))
+            ], icon=ft.Icon(name=ft.Icons.INFO, color=ft.Colors.BLUE)
+        )
+
+        database_error_dialog = ft.AlertDialog(
+            modal=True,
+            title=ft.Text("Database Error"),
+            content=ft.Text("An error occurred while connecting to the database"),
+            alignment=ft.alignment.center,
+            actions=[
+                ft.TextButton("OK", on_click=lambda e: page.close(database_error_dialog))
+            ],
+        )
+        # validation
+        if not username_field.value or not password_field.value:
+            page.open(invalid_input_dialog)
+            page.update()
+            return
+
+
+        try:
+            # establish DB connection
+            connection = connect_db()
+            if not connection:
+                page.open(database_error_dialog)
+                page.update()
+                return
+
+            cursor = connection.cursor(dictionary=True)
+
+            # execute parameterized query
+            cursor.execute(
+                "SELECT * FROM users WHERE username = %s AND password = %s",
+                (username_field.value, password_field.value)
+            )
+
+            result = cursor.fetchone()
+
+            # close DB connection
+            cursor.close()
+            connection.close()
+
+            # check result
+            if result:
+                page.open(success_dialog)
+            else:
+                page.open(failure_dialog)
+
+        except mysql.connector.Error as e:
+            page.open = database_error_dialog
+        
+        page.update()
+    
+    login_button =ft.ElevatedButton(
+            text="Login",
+            width=100,
+            bgcolor=ft.Colors.WHITE,
+            color=ft.Colors.BLUE_500,
+            icon=ft.Icons.LOGIN,
+            on_click=login_click
+        )
+
+    # Add everything to page
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(
+       ft.SafeArea(
+            ft.Container(
+                ft.Column([login_title, username_field, password_field,
+                ft.Container(content=login_button, alignment=ft.alignment.top_right, margin=ft.margin.Margin(0, 20, 40, 0))],
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,), 
+                alignment=ft.alignment.center,
+            ), expand=True
+       )
     )
-
-
-ft.app(main)
+# run app
+ft.app(target=main)
---
+++ week3_labs/week3.md
@@ -0,0 +1,40 @@
+**Student Name:** Joshua Jericho D. Barja
+**Student ID:** 231002307
+**Section:** BSCS-3A
+**Date:** 09-20-2025
+
+## Git Configuration
+
+### Repository Setup
+- **GitHub Repository:** https://github.com/jarichooo/cccs106-projects.git
+- **Local Repository:** ✅ Initialized and connected
+- **Commit History:** 8 commits with descriptive messages
+
+
+## Flet login form
+
+### 1. main.py
+- **Status:** ✅ Completed
+- **Features:** logging into local database, prompting specific dialog messages for errors.
+- **UI Components:** Text, TextField, Buttons, Dialog, Containers
+- **Notes:** I had trouble reading the documentation and its contents, but I was able to pull it off with some help from others and various sources.
+## Technical Skills Developed
+
+### Git Version Control
+
+### Flet GUI Development
+- Flet 0.28.3 syntax and components
+- Page configuration and layout management
+- Event handling and user interaction
+- Modern UI design principles
+
+## Challenges and Solutions
+The problem that I encountered throughout the activity was related to the documentation. There's so much information in a single search. Some functions weren't specific enough.
+
+## Learning Outcomes
+I learned how to properly read and utilize a the documentation. And this realization will likely help me with future endeavours all throughout my journey as a programmer. It was quite a challenge at first since I feel like it's a waste reading the flet documentation, but I was wrong. I spent more time debugging codes that I don't know much about rather than knowing how to actually write a code in flet syntax. It was very troublesome since It's quite frustrating that the solutions I've been writing wasn't working right. The documentation solved every bit of my problem. It's quite considerable a life-changing learning for me.
+## Screenshots
+- ![alt text](lab3_screenshots/login_success.png)
+- ![alt text](lab3_screenshots/login_fail.png)
+- ![alt text](lab3_screenshots/invalid_input.png)
+- ![alt text](lab3_screenshots/database_error.png)
